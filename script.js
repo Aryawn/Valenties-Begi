8d8a5a6d514454c6532f32b0c80e2209840f3c81
Aryawn <frostychillercold@gmail.com>
2026-02-14 00:45:46 +0530

    Yayyy

--- script.js
+++ script.js
@@ -62,7 +62,7 @@ noBtn.addEventListener("mouseover", () => {
 yesBtn.addEventListener("click", () => {
     title.textContent = "Yippeeee!";
 
-    catImg.src = "cat_dance.gif";
+    catImg.src = "seal-scream.gif";
 
     document.querySelector(".letter-window").classList.add("final");
 
